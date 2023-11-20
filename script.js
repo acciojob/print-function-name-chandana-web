5ac49b13f76a3b11374aeecd1a0631e84746d384
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-20 13:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+    var functionName = function() {
+      // Get the function name using arguments.callee.name
+      var currentFunctionName = arguments.callee.name;
+
+      // Display the function name using an alert statement
+      alert('The current function name is: ' + currentFunctionName);
+	}
